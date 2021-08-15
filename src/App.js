09091e44b767fe1f105bd93aaa9cc759ad07fb11
sydney <sydneychase5841@gmail.com>
2021-08-15 15:31:12 -0400

    update components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-// import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import CreateUser from './components/CreateUser';
 import './App.css';
 
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+// import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 function App() {
+  
   return (
     <div className="main">
-       <h1 className="main-header">CREATE MY DAY</h1>
-      <p>Just testing the font to see how it looks</p>
-      <div>
-        <CreateUser />
-      </div>
+      <Navbar />
+     <div className="content">
+       <Home />
+      
+     </div>
+
     </div>
+    
   );
 }
 
--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,34 +1,30 @@
-import { useState } from 'react';
-import { Form } from 'semantic-ui-react'
-
-
-
+import React, { useState } from 'react'
+import { Button, Form } from 'semantic-ui-react'
 
 const CreateUser = () => {
   const [name, setName] = useState('');
-  const [gender, setGender] = useState('');
-  
+    const [gender, setGender] = useState('');
 
-  const postData = () => {
-    console.log(name);
-    console.log(gender)
+    const postData = () => {
+      console.log(name);
+      console.log(gender);
+      
 }
-
     return (
-      <Form>
-        <Form.Group widths='equal'>
-          <Form.Input fluid label='Name' placeholder='Please Type Your Name.' onChange={(e) => setName(e.target.value)}/>
-          <Form.Input
-            fluid
-            label='Gender Identity'
-            // options={options}
-            placeholder='Type Gender Identity Here'
-            onChange={(e) => setGender(e.target.value)}/>
-        </Form.Group>
-        <Form.Button onClick={postData} type='submit'>Submit</Form.Button>
-      </Form>
-    )
-  }
 
+  <Form className="createUser-form">
+    <Form.Field>
+      <label>Name</label>
+      <input placeholder='Type Your Name Here' onChange={(e) => setName(e.target.value)} />
+    </Form.Field>
+    <Form.Field>
+      <label>Gender Identity</label>
+      <input placeholder='Type Gender Identity Here' onChange={(e) => setGender(e.target.value)} />
+    </Form.Field>
+    
+    <Button onClick={postData} type='submit'>Submit</Button>
+  </Form>
+);
+    }
 
 export default CreateUser;
